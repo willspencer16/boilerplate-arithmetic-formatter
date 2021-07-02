f3ac8e5f253e170c000fdddfcb62af7c7513fdaf
Will Spencer <willspencer16@gmail.com>
2021-07-02 14:44:00 +0100

    Program now arranges the first row, arithmetic operator and second row correctly, however, doesn't format depending on number of digits. Yet.

--- arithmetic_arranger.py
+++ arithmetic_arranger.py
@@ -1,9 +1,17 @@
 def arithmetic_arranger(problems):
     first_row = []
+    arithmetic_operator = []
+    second_row = []
 
     for x in problems:
         list = x.split()
         first_row.append(list[0])
+        arithmetic_operator.append(list[1])
+        second_row.append(list[2])
     
-    print(first_row)
-
+    layout = f"   {first_row[0]}      {first_row[1]}      {first_row[2]}      {first_row[3]}\n"\
+            f"{arithmetic_operator[0]} {second_row[0]}    {arithmetic_operator[1]}    {second_row[1]}"\
+            f"    {arithmetic_operator[2]} {second_row[2]}    {arithmetic_operator[3]}  {second_row[3]}\n"\
+            f"-----    ------    ----    -----"
+    
+    return layout
