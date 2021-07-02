69e3efcf0fb7d6860485dc53f3dea7acec6d8953
Will Spencer <willspencer16@gmail.com>
2021-07-02 16:19:37 +0100

    Fixed arithmetic operator error message.

--- arithmetic_arranger.py
+++ arithmetic_arranger.py
@@ -5,31 +5,28 @@ def arithmetic_arranger(problems):
 
     for x in problems:
         list = x.split()
+
         if len(problems) < 5:
             if len(list[0]) < 5:
                 first_row.append(list[0])
             else:
-                print("Error: Numbers cannot be more than four digits.")
-                exit()
+                return "Error: Numbers cannot be more than four digits."
 
-            if list[1] == "+" or "-":
+            if list[1] == "+" or list[1] == "-":
                 arithmetic_operator.append(list[1])
             else:
-                print("Error: Operator must be '+' or '-'.")
-                exit()
+                return "Error: Operator must be '+' or '-'."
             
             if len(list[2]) < 5:
                 second_row.append(list[2])
             else:
-                print("Error: Numbers cannot be more than four digits.")
-                exit()
+                return "Error: Numbers cannot be more than four digits."
         else:
-            print("Error: Too many problems.")
-            exit()
+            return "Error: Too many problems."
     
     arranged_problems = f"   {first_row[0]}      {first_row[1]}      {first_row[2]}      {first_row[3]}\n"\
-            f"{arithmetic_operator[0]} {second_row[0]}    {arithmetic_operator[1]}    {second_row[1]}"\
-            f"    {arithmetic_operator[2]} {second_row[2]}    {arithmetic_operator[3]}  {second_row[3]}\n"\
-            f"-----    ------    ----    -----"
+                        f"{arithmetic_operator[0]} {second_row[0]}    {arithmetic_operator[1]}    {second_row[1]}"\
+                        f"    {arithmetic_operator[2]} {second_row[2]}    {arithmetic_operator[3]}  {second_row[3]}\n"\
+                        f"-----    ------    ----    -----"
     
     return arranged_problems
