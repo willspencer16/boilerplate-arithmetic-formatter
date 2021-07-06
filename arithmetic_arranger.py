2fdb55d13422acace4c5670c01aa31290d5a3b38
Will Spencer <willspencer16@gmail.com>
2021-07-06 11:14:13 +0100

    Refactored and now everything aligns, including when number size differs.

--- arithmetic_arranger.py
+++ arithmetic_arranger.py
@@ -1,60 +1,50 @@
 def arithmetic_arranger(problems):
     first_row = []
-    arithmetic_operator = []
     second_row = []
     dashes = []
-    space = ["    "]
 
-    for x in problems:
-        list = x.split()
+    if len(problems) > 5:
+        return "Error: Too many problems."
 
-        if len(problems) < 5:
-            if len(list[0]) < 5:
+    for problem in range(len(problems)):
+        list = problems[problem].split()
+
+        if len(list[0]) < 5:
+            if len(list[0]) >= len(list[2]):
+                first_row.append("  ")
                 first_row.append(list[0])
+                first_row.append("    ")
             else:
-                return "Error: Numbers cannot be more than four digits."
-
-            if list[1] == "+" or list[1] == "-":
-                arithmetic_operator.append(list[1])
-            else:
-                return "Error: Operator must be '+' or '-'."
-            
-            if len(list[2]) < 5:
-                second_row.append(list[2])
-            else:
-                return "Error: Numbers cannot be more than four digits."
-
-            if len(list[0]) <= len(list[2]):
-                dashes.append("-" * (len(list[2]) + 2))
-            else:
-                dashes.append("-" * (len(list[0]) + 2))
-            
-    for y in first_row:
-        if len(y) == 1:
-            space.append("    ")
-        elif len(y) == 2:
-            space.append("   ")
-        elif len(y) == 3:
-            space.append("  ")
-        elif len(y) == 4:
-            space.append("  ")
-
-    for z in second_row:
-        if len(z) == 1:
-            space.append("    ")
-        elif len(z) == 2:
-            space.append("  ")
-        elif len(z) == 3:
-            space.append(" ")
-        elif len(z) == 4:
-            space.append(" ")
-
+                first_row.append(" " * ((len(list[2]) - len(list[0]) + 2 )))
+                first_row.append(list[0])
+                first_row.append("    ")
         else:
-            return "Error: Too many problems."
+            return "Error: Numbers cannot be more than four digits."
+
+        if list[1] == "+" or list[1] == "-":
+            second_row.append(list[1])
+        else:
+            return "Error: Operator must be '+' or '-'."
+        
+        if len(list[2]) < 5:
+            if len(list[0]) <= len(list[2]):
+                second_row.append(" ")
+                second_row.append(list[2])
+                second_row.append("    ")
+            else:
+                second_row.append(" " * ((len(list[0]) - len(list[2]) + 1 )))
+                second_row.append(list[2])
+                second_row.append("    ")
+        else:
+            return "Error: Numbers cannot be more than four digits."
+
+        if len(list[0]) <= len(list[2]):
+            dashes.append("-" * (len(list[2]) + 2))
+            dashes.append("    ")
+        else:
+            dashes.append("-" * (len(list[0]) + 2))
+            dashes.append("    ")
     
-    arranged_problems = f"{first_row[0]:>5}{first_row[1]:>10}{first_row[2]:>8}{first_row[3]:>9}\n"\
-                        f"{arithmetic_operator[0]}{second_row[0]:>4}{arithmetic_operator[1]:>5}{second_row[1]:>5}"\
-                        f"{arithmetic_operator[2]:>5}{second_row[2]:>3}{arithmetic_operator[3]:>5}{second_row[3]:>4}\n"\
-                        f"{dashes[0]}    {dashes[1]}    {dashes[2]}    {dashes[3]}"
+    arranged_problems = ''.join(first_row) + "\n" + ''.join(second_row) + "\n" + ''.join(dashes)
 
     return arranged_problems
